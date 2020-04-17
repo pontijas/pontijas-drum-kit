41160b3336b237a059d8570e2b0583626c415b85
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-17 20:43:15 +0200

    update semantic names in html and css, add key-playing styles

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('holii');
+// console.log('holii');
+
+const clikedKey = document.querySelector('.js-key');
+
+function pushKey() {
+  console.log('holii');
+}
+
+clikedKey.addEventListener('keydown', pushKey);
